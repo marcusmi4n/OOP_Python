120f5c6fe764a8bb4dba2d614990596b0be0fa83
Marcus Mian <145162436+marcusmi4n@users.noreply.github.com>
2024-09-22 10:15:34 +0300

    Add files via upload

---
+++ Input_Output.py
@@ -0,0 +1,19 @@
+
+
+
+print("What's name?")
+name = input()
+
+print("How old are you?")
+age = input(int)
+
+print("What's your favourite Anime?")
+anime = input()
+
+print("Do you like video games?")
+games = input()
+
+print("NAME: ", name)
+print("AGE: ", age)
+print("ANIME: ", anime)
+print("GAMES: ", games)
