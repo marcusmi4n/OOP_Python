59dc7fb9e0664972ac20dc08cd9391b74f79173e
marcusmi4n <marcuslutas@gmail.com>
2025-07-24 10:15:32 +0300

    python

--- loops.py
+++ loops.py
@@ -7,12 +7,43 @@ print("2 - SUBTRACTION")
 print("3 - MULTIPLICATION")
 print("4 - DIVISION")
 
+print("Enter your choice")
 choice = input()
 
-print("Addition")
-a = int(input())
-b = int(input())
+if choice == "1":
+    print("You have selected - Addition")
+    print("Enter the first number")
+    a = int(input())
+    print("Enter the second number")
+    b = int(input())
 
-result = a + b
-print(result)
+    result = a + b
+    print("ADDITION : ", result)
+elif choice == "2":
+    print("You have selected - Subtraction")
+    print("Enter the first number")
+    a = int(input())
+    print("Enter the second number")
+    b = int(input())
+
+    result = a + b
+    print("SUBTRACTION : ", result)
+elif choice == "2":
+    print("You have selected - Multiplication")
+    print("Enter the first number")
+    a = int(input())
+    print("Enter the second number")
+    b = int(input())
+
+    result = a + b
+    print("MULTIPLICATION : ", result)
+elif choice == "2":
+    print("You have selected - Division")
+    print("Enter the first number")
+    a = int(input())
+    print("Enter the second number")
+    b = int(input())
+
+    result = a + b
+    print("DIVISION : ", result)
 
