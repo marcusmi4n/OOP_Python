7f983461fe9815d4cdfaf6b299ea9c1d596961aa
marcusmi4n <marcuslutas@gmail.com>
2025-08-03 17:16:48 +0300

    debug

--- Django/storefront/playground/views.py
+++ Django/storefront/playground/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def say_hello(request):
     x = 1
     y = 2
-    return render(request, 'hello.html', {'name' : 'Marcus'})
+    return render(request, 'hello.html', {'name' : 'Marcus'}) 
