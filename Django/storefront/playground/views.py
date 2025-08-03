d02122ed19e91cf8d347b8794a6a1cefc5ea6aba
marcusmi4n <marcuslutas@gmail.com>
2025-08-03 18:05:20 +0300

    debug

--- Django/storefront/playground/views.py
+++ Django/storefront/playground/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 
+
+def calculate():
+    x = 1
+    y = 5
+    
+    return x
 # Create your views here.
 def say_hello(request):
-    x = 1
-    y = 2
+    x = calculate()
     return render(request, 'hello.html', {'name' : 'Marcus'}) 
--- Django/storefront/storefront/urls.py
+++ Django/storefront/storefront/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+import debug_toolbar
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls'))
+    path('_debug_/', include(debug_toolbar_urls))
 ]
